d41d29bababebf405584888bf1e89e56eb936b24
Jacie Mae <jcmoninghoff@gmail.com>
2019-11-07 16:21:15 -0800

    Name Inputs Added

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@ int main(int argc, char *argv[]){
     std::string playerName;
     std::string charName;
 
-    int stats[5];
+    int stats[5] = {0,0,0,0,0};
     std::vector<std::string> beliefs;
 
     std::cout << "Lets make a character!" << std::endl;
@@ -12,7 +12,7 @@ int main(int argc, char *argv[]){
     std::cout << "First, what's YOUR name?" << std::endl;
 
     bool correct = false;
-    char in;
+    std::string in;
 
     do{
         std::cin >> playerName;
@@ -22,10 +22,10 @@ int main(int argc, char *argv[]){
 
         do{
         std::cin >> in;
-            if(in == 'N' || in == 'n'){
+            if(in == "N" || in == "n"){
                 break;
             }
-            else if(in == 'Y' || in == 'y'){
+            else if(in == "Y" || in == "y"){
                 correct = true;
                 break;
             }
@@ -36,23 +36,61 @@ int main(int argc, char *argv[]){
         }while(!correct);
     }while(!correct);
 
-    std::cout << "Lovely! Welcome, " << playerName << std::endl;
+    std::cout << "Lovely! Welcome, " << playerName << "." << std::endl;
 
     std::cout << "Now, lets get started on your character." << std::endl;
 
-    std::cout << "Do you have a name in mind for your character?" << std::endl;
+    std::cout << "Do you have a name in mind for your character? You may also set a name later. Y/N" << std::endl;
+    correct = false;
+    bool hasCharName = false;
     do{
         std::cin >> in;
 
-        if(in == 'N' || in == 'n'){
+        if(in == "N" || in == "n"){
+            hasCharName = false;
+            break;
         }
-        else if(in == 'Y' || in == 'y'){
-            correct = true;
+        else if(in == "Y" || in == "y"){
+            hasCharName = true;
             break;
         }
         else{
-            std::cout << "buh" << std::endl;
+            std::cout << "I'm sorry, '" << in << "' is not a valid input" << std::endl;
+            std::cout << "Please input 'Y' if you have a name in mind, and 'N' if you would like to input a name later" << std::endl;
         }
     }while(1 == 1);
+
+    if(hasCharName){
+        
+        std::cout << "Great! Please input your desired Character's name now." << std::endl;
+        correct = false;
+        do{
+            std::cin >> charName;
+
+            std::cout << "Your Character's name is: " << charName << std::endl;
+            std::cout << "Is this the name you'd like to go with? Y/N" << std::endl;
+
+            do{
+            std::cin >> in;
+                if(in == "N" || in == "n"){
+                    std::cout << "Please input another name" << std::endl;
+                    break;
+                }
+                else if(in == "Y" || in == "y"){
+                    correct = true;
+                    break;
+                }
+                else{
+                    std::cout << "I'm sorry, '" << in << "' is not a valid input" << std::endl;
+                    std::cout << "Please input 'Y' if your name is correct, and 'N' if you would like to input a different name" << std::endl;
+                }
+            }while(!correct);
+        }while(!correct);
+    }
+
+    std::cout << "Time for the fun part: a quiz!" << std::endl << "Fear not, this quiz isn't graded. It will, however, be used to determine your character's starting stats." << std::endl << "Answer wisely!" << std::endl;
+    
+    
+    std::cout << "Something outside" << std::endl;
 
 }
