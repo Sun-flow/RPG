282cb225b3a8aa82babe00ace64e4835ef507465
Jacie Mae <jcmoninghoff@gmail.com>
2019-11-14 19:30:11 -0800

    Updated Data Intake

--- include/Character.h
+++ include/Character.h
@@ -6,24 +6,43 @@
 #include "Injuries.h"
 
 
+
+
 class Character {
     private:
         std::string playerName;
         std::string charName;
-        int stats[5];
+        int stats[5]; //Authority, Focus, Sharp, Empathy, Hunger
+        struct nature{
+            std::string name;
+            int value;
+        };
+        struct pairs{
+            nature first;
+            nature second;
+        };
+        pairs nature[3]; //(Chaotic - Lawful), (Impulsive - Stoic), (Violent - Peaceful), (Introvert - Extrovert), (Spiritual, Pragmatic)
+        //Lawfullness, Impulsiveness, Violence, Extroversion, Spirituality
         std::vector<std::string> beliefs;
         
         Injuries injuries;
     public:
         Character();
-        Character(std::string inPlayerName, std::string inCharName, int inStatArray[], std::vector<std::string> inBeliefs);
+        Character(std::string inPlayerName, std::string inCharName, int inStatArray[]);
+        void toString();
+        void printStats();
+        void constructPairs();
+        void constructPair(int which, std::string first, std::string second);
         void setPlayerName(std::string inName);
         void setCharName(std::string inName);
-        void setStats(int stats[]);
+        void setStats(std::vector< int > inStats);
+        void changeStat(std::vector< int > which, std::vector< int > modify);
+        void changeStat(int which, int modify);
+        void changeAlignment(std::vector< int > which, std::vector< int > modify);
         void setBelief(int beliefNum, std::string belief);
         void setInjuries(std::vector< std::vector< bool > > inInjuries);
         void addBelief(std::string inBelief);
-        void changeStat(int statNum, int newStat);
+        //void changeStat(int statNum, int newStat);
         void addInjury(int severity, std::string description);
         
         //void endSession(text.file);
--- src/Character.cpp
+++ src/Character.cpp
@@ -1,11 +1,11 @@
 #include "Character.h"
 #include <vector>
+#include <iostream>
 
 Character::Character(){
     setPlayerName("");
     setCharName("");
-    int inStats[5] = {0,0,0,0,0};
-    setStats(inStats);
+    setStats({0,0,0,0,0,0});
     //beliefs = new std::vector< std::vector < std::string > >;
     //instincts = null;
 }
@@ -14,11 +14,40 @@ Character::Character(){
 
 //}
 
-Character::Character(std::string inPlayerName, std::string inCharName, int inStatsArray[5], std::vector<std::string> inBeliefs){
+Character::Character(std::string inPlayerName, std::string inCharName, int inStatsArray[5]){
     setPlayerName(inPlayerName);
     setCharName(inCharName);
     setStats(inStatsArray);
-    //setBeliefs(inBeliefs);
+    constructPairs();
+}
+
+void Character::constructPairs(){
+    constructPair(0, "Chaotic", "Lawful");
+    constructPair(1, "Impulsive", "Stoic");
+    constructPair(2, "Violent", "Peaceful");
+    constructPair(3, "Introver", "Extrovert");
+    constructPair(4, "Spiritual", "Pragmatic");
+}
+
+void Character::constructPair(int which, std::string first, std::string second){
+    nature[which].first = {first, 0};
+    nature[which].second = {second, 0};
+}
+
+void Character::toString(){
+    std::cout << "Player: " << playerName << std::endl;
+    std::cout << "Character: " << charName << std::endl;
+    std::cout << "--------Stats--------" << std::endl;
+    printStats();
+    std::cout << "---------------------" << std::endl;
+}
+
+void Character::printStats(){
+    std::cout << "  Authority:      " << stats[0] << std::endl;
+    std::cout << "  Focus:          " << stats[1] << std::endl;
+    std::cout << "  Sharp:          " << stats[2] << std::endl;
+    std::cout << "  Empathy:        " << stats[3] << std::endl;
+    std::cout << "  Hunger:         " << stats[4] << std::endl;
 }
 
 void Character::setPlayerName(std::string inName){
@@ -29,10 +58,36 @@ void Character::setCharName(std::string inName){
     charName = inName;
 }
 
-void Character::setStats(int inStats[]){
-    for(int i = 0; i < 5; i++){ //use array size later
-        stats[i] = inStats[i];
+void Character::setStats(std::vector< int > inStats){
+    int i = 0;
+    while(!inStats.empty()){ //use array size later
+        stats[i] = inStats.back();
+        inStats.pop_back();
+        i++;
     }
+}
+
+void Character::changeStat(std::vector< int > which, std::vector < int > modify){
+    while(!which.empty()){
+        stats[which.back() - 1] += modify.back();
+        which.pop_back();
+        modify.pop_back();
+    }
+
+}
+
+void Character::changeAlignment(std::vector< int > which, std::vector < int > modify){
+    while(!which.empty()){
+        if(modify.back() == 0){
+            nature[which.back() - 1].first.value += 1;
+        }
+        else{
+            nature[which.back() - 1].second.value += 1;
+        }
+        which.pop_back();
+        modify.pop_back();
+    }
+
 }
 
 void Character::setBelief(int beliefNum, std::string belief){
